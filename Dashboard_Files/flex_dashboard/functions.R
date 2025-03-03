bec0d01cfce8cf4077bc41bb0c7e31624fd30033
John Lee <swannyy.stat@gmail.com>
2025-03-03 16:05:37 -0600

    Reorganize and modularize code

---
+++ Dashboard_Files/flex_dashboard/functions.R
@@ -0,0 +1,185 @@
+load_foia <- function(input_data, possible_agencies, section_name) {
+  if (!(input_data %in% possible_agencies)) {
+    stop("Invalid agency selected")
+  }
+  
+  load(str_glue("{input_data}_data.Rda", envir = .GlobalEnv))
+  return(foia_data[[section_name]])
+}
+
+plot_single_column <- function(data, column_name, selected_components, selected_years, manual_titles = NULL) {
+  
+    # print(head(data))
+    # print(column_name)
+    # print(selected_components)
+    # print(selected_years)
+    # print(manual_titles)
+  
+  if (is.null(manual_titles)) {
+    # Use automated titles
+    plot_data <- data %>%
+      filter(OrganizationAbbreviationText %in% selected_components, FY %in% selected_years) %>%
+      select(FY, OrganizationAbbreviationText, !!sym(column_name)) %>%
+      mutate(!!sym(column_name) := as.numeric(!!sym(column_name)))
+    
+    y_lab <- column_name
+  } else {
+    # Use manually defined titles
+    plot_data <- data %>%
+      filter(OrganizationAbbreviationText %in% selected_components, FY %in% selected_years) %>%
+      select(FY, OrganizationAbbreviationText, !!sym(column_name)) %>%
+      mutate(!!sym(column_name) := as.numeric(!!sym(column_name)))
+    
+    y_lab <- manual_titles
+  }
+  
+  plot_data <- na.omit(plot_data)
+  
+  plot <- ggplot(plot_data, aes(x = FY, y = !!sym(column_name), color = OrganizationAbbreviationText, group = OrganizationAbbreviationText)) +
+    geom_line(aes(text = paste("FY:", FY, "<br>", y_lab, ":", !!sym(column_name), "<br>Component:", OrganizationAbbreviationText))) +
+    geom_point(aes(text = paste("FY:", FY, "<br>", y_lab, ":", !!sym(column_name), "<br>Component:", OrganizationAbbreviationText))) +
+    labs(title = paste(y_lab, "for Selected Components"),
+         y = y_lab,
+         x = "FY",
+         color = "Component") +
+    theme_minimal() +
+    theme(legend.position = "top") 
+  
+  
+  return(ggplotly(plot, tooltip = "text")) #removing the auto-generated tooltip and opting for above
+}
+
+
+
+plot_two_columns <- function(data, selected_columns, selected_years) {
+  selected_columns_expr <- rlang::syms(selected_columns)
+  
+  plot_data <- data %>%
+    filter(Year %in% selected_years) %>%
+    select(Year, !!!selected_columns_expr) %>%
+    pivot_longer(cols = -Year, names_to = "Metric", values_to = "Value")
+  
+  plot_data$Value <- as.numeric(gsub(",", "", plot_data$Value))
+  
+  plot_data <- na.omit(plot_data)
+  
+  # Create a line plot using ggplot2
+  plot <- ggplot(plot_data, aes(x = Year, y = Value/1e9, color = Metric)) +
+    geom_point() +
+    geom_line() +
+    labs(title = "Budget Metrics",
+         y = "Billions of Dollars ($)",
+         x = "Year") +
+    theme_minimal() +
+    theme(legend.position = "top")
+  
+  return(ggplotly(plot))
+}
+
+plot_single_column_budget <- function(data, column_to_plot, selected_years) {
+  plot_data <- data %>%
+    filter(Year %in% selected_years) %>%
+    select(Year, {{ column_to_plot }}) %>%
+    pivot_longer(cols = -Year, names_to = "Metric", values_to = "Value")
+  
+  plot_data <- na.omit(plot_data)
+  
+  # Convert values to percentages
+  plot_data$Value <- plot_data$Value * 100
+  
+  # Create a line plot using ggplot2
+  plot <- ggplot(plot_data, aes(x = Year, y = Value, color = Metric, group = Metric)) +
+    geom_point() +
+    geom_line() +
+    labs(title = paste("FOIA Budget / Total Budget (%)"),
+         y = paste("FOIA/Total Budget (%)"),
+         x = "Year") +
+    theme_minimal() +
+    theme(legend.position = "top")
+  
+  return(ggplotly(plot))
+}
+
+plot_single_column_backlog <- function(data, column_to_plot_backlog, selected_years) {
+  plot_data <- data %>%
+    filter(Year %in% selected_years) %>%
+    select(Year, {{ column_to_plot_backlog }}) %>%
+    pivot_longer(cols = -Year, names_to = "Metric", values_to = "Value")
+  
+  plot_data <- na.omit(plot_data)
+  
+  # Create a line plot using ggplot2
+  plot <- ggplot(plot_data, aes(x = Year, y = Value, color = Metric, group = Metric)) +
+    geom_point() +
+    geom_line() +
+    labs(title = paste("Component Backlog"),
+         y = paste("Backlogged Requests"),
+         x = "Year") +
+    theme_minimal() +
+    theme(legend.position = "top")
+  
+  return(ggplotly(plot))
+}
+
+
+plot_stacked_area <- function(data, column_names, selected_components, selected_years) {
+  plot_data <- data %>%
+    filter(OrganizationAbbreviationText %in% selected_components, FY %in% selected_years) %>%
+    select(FY, OrganizationAbbreviationText, !!!syms(column_names)) %>%
+    mutate(across(all_of(column_names), as.numeric))
+  
+  plot <- ggplot(plot_data, aes(x = FY, y = !!sym(column_names[1]), fill = OrganizationAbbreviationText)) +
+    geom_area(position = "stack") +
+    labs(title = paste("Stacked Area Chart of Selected Components"),
+         y = "Value",
+         x = "FY",
+         fill = "OrganizationAbbreviationText",) +
+    theme_minimal() +
+    theme(legend.position = "top") 
+  
+  return(ggplotly(plot))
+}
+
+plot_two_columns_twoaxes <- function(data, selected_columns, selected_years) {
+  selected_columns_expr <- rlang::syms(selected_columns)
+  
+  plot_data <- data %>%
+    filter(Year %in% selected_years) %>%
+    select(Year, !!!selected_columns_expr)
+  
+  #print(plot_data)
+  
+  for (col_name in selected_columns) {
+    plot_data[[col_name]] <- as.numeric(gsub(",", "", plot_data[[col_name]]))
+  }
+  
+  r1 <- diff(range(plot_data[[selected_columns[1]]]))
+  
+  r2 <- diff(range(plot_data[[selected_columns[2]]]))
+  
+  coeff <- r2/r1
+  
+  plot_data[[selected_columns[2]]] <- plot_data[[selected_columns[2]]]/coeff
+  
+  #plot_data <- na.omit(plot_data)
+  
+  
+  plot <- ggplot(plot_data, aes(x = Year)) +
+    geom_point(aes_string(y = selected_columns[1]), color = "blue") +
+    geom_line(aes_string(y = selected_columns[1]), color = "blue") +
+    geom_point(aes_string(y = selected_columns[2]), color = "red") +
+    geom_line(aes_string(y = selected_columns[2]), color = "red") +
+    scale_y_continuous(name = selected_columns[1], sec.axis = sec_axis(~.*coeff, name = selected_columns[2])) +
+    labs(title = "Budget Metrics",
+         y = "USD ($)",
+         x = "Year") +
+    theme_minimal() +
+    theme(legend.position = "top",
+          axis.title.y = element_text(color = "blue"),     # Color for y1-axis title
+          axis.title.y.right = element_text(color = "red") # Color for y2-axis title
+    )
+  
+  
+  return(plot)
+}
+
---
+++ Dashboard_Files/flex_dashboard/ui_functions.R
@@ -0,0 +1,55 @@
+ui_function <- function(
+    section_agency_id, section_component_id, section_fy_id,
+    custom_column_select_ui) {
+  
+  tagList(
+    # 
+    HTML(
+      "<br/><br/><p>
+      The dashboard displays the data from the Freedom of Information Act (FOIA) Annual Reports released each year by federal agencies and their components. 
+      The sources for the department and component annual budgets data are available <a href=\"https://www.justice.gov/oip/reports-1\">here</a>. The sections of the Annual Reports are accessible via the top panel.
+      </p>
+      <p>Please select agency, component/region, data type, and fiscal year below.</p>"
+    ), 
+    # Agency selection
+    selectInput(section_agency_id, "Agency", 
+                choices = c("DHS", "EPA", "DOL", "DOJ", "DOS", "HHS"), 
+                selected = "DHS"),
+    
+    # Data (column) selection
+    custom_column_select_ui,
+    
+    # Component selection
+    checkboxGroupInput(
+      section_component_id,
+      "Component",
+      choices = c("CBP", "PRIV", "USCIS", "ICE", "OIG", "CRCL"), 
+      selected = c("CBP", "PRIV")
+    ),
+    
+    checkboxGroupInput(
+      section_fy_id, "FY", 
+      choices = c("10", "11", "12", "13", "14", "15", 
+                  "16", "17", "18", "19", "20", "21", 
+                  "22","23"), 
+      selected = c("10", "11", "12", "13", "14", "15", 
+                   "16", "17", "18")),
+    
+    HTML(
+      "<br/>For more information about the FOIA, see [foia.gov](https://www.foia.gov).
+      <br/>
+  Created by [Kendall McKay](https://deportation-research.buffett.northwestern.edu/people/) for the [Deportation Research Clinic](https://deportation-research.buffett.northwestern.edu).
+  <br/>
+  <br/>
+  <br/>
+  Project support provided by [Aaron Geller](https://faculty.wcas.northwestern.edu/aaron-geller/) and [Patrick Zacher](https://www.linkedin.com/in/patrick-zacher/) of Northwestern University's [IT Research Computing and Data Services (RCDS)](https://www.it.northwestern.edu/departments/it-services-support/research/).
+  <br/>
+  <br/>
+  <br/>
+  Funding provided by the Northwestern University [Institute for Policy Research (IPR)](https://www.ipr.northwestern.edu).
+  
+  <br/>
+  <div style=\"font-size: smaller;\">Jacqueline Stevens and Kendall McKay, FOIA Dashboard, https:/deportationresearch.shinyapps.io/FOIAdashboard/ 
+  Copyright: <a href=\"https://creativecommons.org/licenses/by-nc/4.0/\">CC BY-NC</a> </div>"
+    ))
+}
