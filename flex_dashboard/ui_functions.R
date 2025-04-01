9863436b54608f81e59b39a613c3075c1e876a25
John Lee <swannyy.stat@gmail.com>
2025-04-01 15:48:11 -0500

    Update text for agency budgets tab

--- flex_dashboard/ui_functions.R
+++ flex_dashboard/ui_functions.R
@@ -1,8 +1,62 @@
+sidebar_text_list_1 <- list(
+  top = HTML(
+    "<br/><br/><p>
+      The dashboard displays the data from the Freedom of Information Act (FOIA) Annual Reports released each year by federal agencies and their components. 
+      The sources for the department and component annual budgets data are available <a href=\"https://www.justice.gov/oip/reports-1\">here</a>. The sections of the Annual Reports are accessible via the top panel.
+      </p>
+      <p>Please select data type, component agency(s), and fiscal year(s) below.</p>"
+  ),
+  bottom = HTML(
+    "
+    <br/>For more information about the FOIA, see <a href=\"https://www.foia.gov\">foia.gov</a>.
+    <br/>
+    <br/>
+    Created by <a href=\"https://deportation-research.buffett.northwestern.edu/people/\">Kendall McKay</a> for the <a href=\"https://deportation-research.buffett.northwestern.edu\">Deportation Research Clinic</a>.
+    <br/>
+    <br/>
+    Project support provided by <a href=\"https://faculty.wcas.northwestern.edu/aaron-geller/\">Aaron Geller</a> and <a href=\"https://www.linkedin.com/in/patrick-zacher/\">Patrick Zacher</a> of Northwestern University's <a href=\"https://www.it.northwestern.edu/departments/it-services-support/research/\">IT Research Computing and Data Services (RCDS)</a>.
+    <br/>
+    <br/>
+    Funding provided by the Northwestern University <a href=\"https://www.ipr.northwestern.edu\">Institute for Policy Research (IPR)</a>.
+    <br/>
+    <br/>
+    <div style=\"font-size: smaller;\">Jacqueline Stevens and Kendall McKay, FOIA Dashboard, <a href=\"https:/deportationresearch.shinyapps.io/FOIAdashboard/\">https:/deportationresearch.shinyapps.io/FOIAdashboard/</a>
+    Copyright: <a href=\"https://creativecommons.org/licenses/by-nc/4.0/\">CC BY-NC</a> </div>
+    "
+  )
+)
+
+sidebar_text_list_budgets <- list(
+  top = HTML(
+    "
+    <br>
+    <br>
+    <br>
+    The <a href=\"https://deportation-research.buffett.northwestern.edu/\">Deportation Research Clinic's</a>
+    dashboard displays the data from the Freedom of Information Act (FOIA) Annual Reports released 
+    each year by federal agencies and their components. Access the Annual Reports 
+    <a href=\"https://www.justice.gov/oip/reports-1\">here</a>. The dashboard's tabs reflect several of the categories of the Annual Reports.
+    <br>
+    <br>
+    The sources for the budget data are accessible <a href=\"https://docs.google.com/spreadsheets/d/1CV6Z_gTZ0KVZ84ChH9lS88vAB-SyuqvRFO_Mavl7NDw/edit?usp=sharing\">here</a>.
+    <br>
+    <br>
+    Please select data type, component agency(s), and fiscal year(s) below.
+    <br>
+    <br>
+    "
+  ),
+  bottom = HTML(
+    "<br/>For more information about the FOIA, see <a href\"https://www.foia.gov\">foia.gov</a>."
+  )
+)
+
 ui_function <- function(
     section_agency_id, 
     section_component_id, 
     section_fy_id,
-    custom_column_select_ui, 
+    custom_column_select_ui,
+    sidebar_text_list,
     agencies = c("DHS", "EPA", "DOL", "DOJ", "DOS", "HHS"),
     component_select_ui = NULL) {
   
@@ -17,14 +71,9 @@ ui_function <- function(
   }
   
   tagList(
-    # 
-    HTML(
-      "<br/><br/><p>
-      The dashboard displays the data from the Freedom of Information Act (FOIA) Annual Reports released each year by federal agencies and their components. 
-      The sources for the department and component annual budgets data are available <a href=\"https://www.justice.gov/oip/reports-1\">here</a>. The sections of the Annual Reports are accessible via the top panel.
-      </p>
-      <p>Please select agency, component/region, data type, and fiscal year below.</p>"
-    ), 
+    # Top Text
+    sidebar_text_list$top, 
+    
     # Agency selection
     selectInput(section_agency_id, "Agency", 
                 choices = agencies, 
@@ -44,21 +93,7 @@ ui_function <- function(
       selected = c("10", "11", "12", "13", "14", "15", 
                    "16", "17", "18")),
     
-    HTML(
-      "<br/>For more information about the FOIA, see [foia.gov](https://www.foia.gov).
-      <br/>
-  Created by [Kendall McKay](https://deportation-research.buffett.northwestern.edu/people/) for the [Deportation Research Clinic](https://deportation-research.buffett.northwestern.edu).
-  <br/>
-  <br/>
-  <br/>
-  Project support provided by [Aaron Geller](https://faculty.wcas.northwestern.edu/aaron-geller/) and [Patrick Zacher](https://www.linkedin.com/in/patrick-zacher/) of Northwestern University's [IT Research Computing and Data Services (RCDS)](https://www.it.northwestern.edu/departments/it-services-support/research/).
-  <br/>
-  <br/>
-  <br/>
-  Funding provided by the Northwestern University [Institute for Policy Research (IPR)](https://www.ipr.northwestern.edu).
-  
-  <br/>
-  <div style=\"font-size: smaller;\">Jacqueline Stevens and Kendall McKay, FOIA Dashboard, https:/deportationresearch.shinyapps.io/FOIAdashboard/ 
-  Copyright: <a href=\"https://creativecommons.org/licenses/by-nc/4.0/\">CC BY-NC</a> </div>"
-    ))
+    # Bottom Text
+    sidebar_text_list$bottom
+  )
 }
